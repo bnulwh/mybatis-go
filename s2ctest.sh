5265ad1ef28097af25628042a1c95ba6c31bb599
liuwenhui <qdn163@163.com>
2022-11-09 22:34:15 +0800

    log format

---
+++ s2ctest.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+./schema2code --db nextcloud --host tdsql-ac8g7bce.sql.tencentcdb.com --port 14 --username ncloud --password ncloud@Huik2020 --prefix oc_ --tables oc_accounts,oc_cards
